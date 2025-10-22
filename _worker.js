be9b14a811d0501e9c73a442edbc6f4e1ccfa3d2
client-proo <client.proo2024@gmail.com>
2025-10-23 02:26:44 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,5 +1,5 @@
 // دامنه ورکرتون یا نیم بهارو اینجا بزارید 
-const Domain = 'https://cfnimer.446230.ir.cdn.ir'; 
+const Domain = 'https://free.117016.ir.cdn.ir'; 
 
 // تنظیمات احراز هویت HTTP
 const AUTH_ENABLED = false; // تغییر به true یا false برای فعال/غیرفعال کردن احراز هویت
